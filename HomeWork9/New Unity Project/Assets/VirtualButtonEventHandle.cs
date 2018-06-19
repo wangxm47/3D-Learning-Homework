e2aff71a6d9979cef58179e16f4d52e844f08ede
wangxm47 <31608788+wangxm47@users.noreply.github.com>
2018-06-19 22:22:33 +0800

    HomeWork9

---
+++ HomeWork9/New Unity Project/Assets/VirtualButtonEventHandle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonEventHandle : MonoBehaviour ,IVirtualButtonEventHandler{
+
+    public GameObject vb;
+    public Animator ani;
+	// Use this for initialization
+	void Start () {
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        //Debug.Log(vbb);
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        if(ani.GetInteger("state") == 1)
+        {
+            ani.SetInteger("state", 0);
+        }
+        else
+        {
+            ani.SetInteger("state", 1);
+        }
+        Debug.Log("pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("released");
+    }
+}
